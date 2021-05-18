6d131399a834f4ad81a5a7640ba7cb2f5a5c26cc
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-18 21:39:40 +0100

    Implemented changes to the controller and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+flat_name = Faker::Hipster.sentence
+desc = Faker::Hipster.paragraph
+address = Faker::Address.street_address
+price = Faker::Number.decimal(l_digits: 2)
+guests = Faker::Number.number(digits: 2)
+
+10.times do
+  flat = Flat.new(name: flat_name, address: address, description: desc, price_per_night: price, number_of_guests: guests)
+  flat.save if flat.valid?
+end
